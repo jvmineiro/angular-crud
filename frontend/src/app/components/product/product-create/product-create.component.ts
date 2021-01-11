ee2fd2d114853e6f86552bba198e55afbcc1f35f
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-01-11 09:39:38 -0300

    quase finalizado o Method Criar PRoduto

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../product.model';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
@@ -9,6 +10,11 @@ import { ProductService } from '../product.service';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: 'Produto de Teste',
+    price: 125.98
+  }
+
   constructor(private productService: ProductService,
     private router: Router) { }
 
@@ -17,7 +23,10 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void {
-    this.productService.showMessage('Produto Criado!')
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto Criado!')
+      this.router.navigate(['/products'])
+    })
   }
 
   cancel(): void {
